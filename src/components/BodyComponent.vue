9c0eae10a77a529a3fdccc5513e858f53558efa9
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-11-14 17:58:00 +0100

    founded component created

--- src/components/BodyComponent.vue
+++ src/components/BodyComponent.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container-md">
+  <div class="container-md my-body">
+    <section class="my-3">
+      <FoundedComponent :num="length" />
+    </section>
     <section class="cards row row-cols-lg-5 g-3 row-cols-md-4 row-cols-sm-3">
       <CardComponent v-for="(char, index) in characterList" :key="char.char_id" :character="char" />
     </section>
@@ -9,21 +12,25 @@
 <script>
 import CardComponent from './CardComponent.vue';
 import axios from 'axios';
+import FoundedComponent from './FoundedComponent.vue';
 
 
 export default {
   name: "BodyComponent",
   components: {
-    CardComponent
+    CardComponent,
+    FoundedComponent
   },
   data() {
     return {
       characterList: [],
+      length: null,
     }
   },
   created() {
     axios.get('https://breakingbadapi.com/api/characters').then((answ) => {
       this.characterList = answ.data;
+      this.length = this.characterList.length;
     })
   }
 
@@ -34,8 +41,12 @@ export default {
 @use '../assets/style/partials/mixins' as*;
 @use '../assets/style/partials/variables' as*;
 
-.cards {
-  padding: 2rem;
+.my-body {
   background-color: $white-text;
+  padding: 2rem;
+
+  .cards {
+    padding: 2rem;
+  }
 }
 </style>
--- src/components/FoundedComponent.vue
+++ src/components/FoundedComponent.vue
@@ -1,14 +1,23 @@
 <template>
-  <div></div>
+  <div class="found">Founded {{ num }} characters</div>
 </template>
 
 <script>
 export default {
   name: 'FoundedComponent',
-
+  props: {
+    num: Number,
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/style/partials/mixins' as*;
+@use '../assets/style/partials/variables' as*;
 
+.found {
+  padding: 1.2rem;
+  font-weight: 600;
+  background-color: $found-bg;
+}
 </style>
